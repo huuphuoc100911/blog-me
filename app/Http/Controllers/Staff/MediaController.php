6dd89f5e6af51d95f265bdb4beaff146d83135c7
HuuPhuoc <nguyenvanhuuphuoc17t1@gmail.com>
2022-11-14 17:04:55 +0700

    staff_media_v1

---
+++ app/Http/Controllers/Staff/MediaController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers\Staff;
+
+use App\Http\Controllers\Controller;
+use App\Services\Admin\CategoryService;
+use App\Services\Staff\MediaService;
+use Illuminate\Http\Request;
+
+class MediaController extends Controller
+{
+    public function __construct(MediaService $mediaService, CategoryService $categoryService)
+    {
+        $this->mediaService = $mediaService;
+        $this->categoryService = $categoryService;
+    }
+    public function index()
+    {
+        $staffId = auth('staff')->user()->id;
+        $medias = $this->mediaService->getListMedia($staffId);
+
+        return view('staff.media.index', compact('medias'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $categories = $this->categoryService->getListCategory();
+
+        return view('staff.media.create', compact('categories'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
