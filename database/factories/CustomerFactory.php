369cda1e348fc7ea777e15b4454eca0db280ca51
huuphuoc100911 <nguyenvanhuuphuoc17t1@gmail.com>
2023-10-11 11:14:24 +0700

    seeder factory

---
+++ database/factories/CustomerFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CustomerFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'address' => $this->faker->address(),
+            'phone_number' => $this->faker->phoneNumber(),
+            'city' => $this->faker->city(),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(CustomerSeeder::class);
+
         DB::table('staffs')->insert([
             [
                 'name' => 'Staff',
