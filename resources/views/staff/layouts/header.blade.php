951b1cc4ff94c772c1c22b8c69cd6b18f9e9fa29
huuphuoc100911 <nguyenvanhuuphuoc17t1@gmail.com>
2023-11-14 21:53:07 +0700

    a

--- resources/views/staff/layouts/header.blade.php
+++ resources/views/staff/layouts/header.blade.php
@@ -22,6 +22,7 @@
 >
   <head>
     <meta charset="utf-8" />
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
     <meta
       name="viewport"
       content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
