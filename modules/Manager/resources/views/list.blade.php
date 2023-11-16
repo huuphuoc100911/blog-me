94f3479dc909e86fad4224befdddcb3c8f7e31b2
huuphuoc100911 <nguyenvanhuuphuoc17t1@gmail.com>
2023-11-16 22:44:34 +0700

    setup manager module

---
+++ modules/Manager/resources/views/list.blade.php
@@ -0,0 +1,5 @@
+@extends('manager.layouts.layout')
+@section('page-title', 'Quản lý người dùng')
+@section('content')
+Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum tenetur, labore impedit dolores beatae minus ex, nostrum modi minima veritatis architecto possimus quam eius molestiae inventore eos repudiandae rem voluptatum!
+@endsection
---
+++ modules/Manager/src/Repositories/ManagerRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Repositories\Manager;
+
+use App\Repositories\BaseRepository;
+use Modules\Manager\src\Repositories\ManagerRepositoryInterface;
+
+class ManagerRepository extends BaseRepository implements ManagerRepositoryInterface
+{
+    public function getModel()
+    {
+    }
+}
