8339a2f0ee305bb5b257a59e40ec303781870c4e
huuphuoc100911 <nguyenvanhuuphuoc17t1@gmail.com>
2023-11-16 23:02:08 +0700

    custome name module

--- modules/CManager/src/Http/Controllers/UserController.php
+++ modules/CManager/src/Http/Controllers/UserController.php
@@ -1,14 +1,13 @@
 <?php
 
-namespace Modules\Manager\src\Http\Controllers;
+namespace Modules\CManager\src\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 
 class UserController extends Controller
 {
-
     public function index()
     {
-        return view('Manager::list');
+        return view('CManager::list');
     }
 }
