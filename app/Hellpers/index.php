1fb0c5a08f3ab0c20c5c74ff7b18211b01c8f64d
huuphuoc100911 <nguyenvanhuuphuoc17t1@gmail.com>
2023-11-18 11:20:39 +0700

    config function

--- app/Hellpers/index.php
+++ app/Hellpers/index.php
@@ -132,3 +132,20 @@ function checkIsRoleIsset($dataArr, $moduleName, $role = 'view')
 
     return false;
 }
+
+function getCategories($categories, $old = '', $parentId = 0, $char = '')
+{
+    $id = request()->route()->category;
+
+    if ($categories) {
+        foreach ($categories as $key => $category) {
+            $selected = $old == $category->id ? 'selected' : '';
+
+            if ($category->parent_id == $parentId && $id != $category->id) {
+                echo '<option value="' . $category->id . '"' . $selected . '>' . $char . $category->name . '</option>';
+                unset($categories[$key]);
+                getCategories($categories, $old, $category->id, $char . '--');
+            }
+        }
+    }
+}
