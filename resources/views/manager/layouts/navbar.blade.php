03858c018106e0de5f6e4d8cc6867b82b1960fb0
huuphuoc100911 <nguyenvanhuuphuoc17t1@gmail.com>
2023-11-19 21:32:56 +0700

    a

--- resources/views/manager/layouts/navbar.blade.php
+++ resources/views/manager/layouts/navbar.blade.php
@@ -34,7 +34,7 @@
             <i class="fas fa-fw fa-cog"></i>
             <span>Components</span>
         </a>
-        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+        <div id="collapseTwo" class="collapse show" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
                 <h6 class="collapse-header">Custom Components:</h6>
                 <a class="collapse-item" href="{{ route('manager.user.index') }}">User</a>
